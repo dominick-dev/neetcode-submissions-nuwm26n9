ccc93b147ded51ba6a61a3d1d66af5a4e28fc764
Dominick DeVincenzo <98797060+dominick-dev@users.noreply.github.com>
2026-04-07 21:59:58 -0400

    Add: single-number - submission-0

---
+++ Data Structures & Algorithms/single-number/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        seen = set()
+
+        for n in nums:
+            if n in seen:
+                seen.remove(n)
+            else:
+                seen.add(n)
+        
+        return seen.pop()
+                
+        
