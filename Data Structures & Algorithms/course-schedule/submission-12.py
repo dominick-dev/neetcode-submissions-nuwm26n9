7b95e2ecd39513a26fc4cb2e158e4ec052d35491
Dominick DeVincenzo <98797060+dominick-dev@users.noreply.github.com>
2026-04-23 13:28:32 -0400

    Add: course-schedule - submission-12

---
+++ Data Structures & Algorithms/course-schedule/submission-12.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # make adj list
+        mp = {}
+        for c, p in prerequisites:
+            if c not in mp:
+                mp[c] = []
+            mp[c].append(p)
+        
+        visited = set()
+        def dfs(c):
+            # go through neighbors of course and look for cycle
+            # found cycle return false
+            # otherwise can continue
+            if c in visited:
+                return False
+            if c not in mp or len(mp[c]) == 0:
+                mp[c] = []
+                return True
+            visited.add(c)
+            for neighbor in mp[c]:
+                if not dfs(neighbor):
+                    return False
+            visited.remove(c)
+            return True
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        
+        return True
+            
+
